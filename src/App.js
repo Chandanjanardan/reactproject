af892d66066f4a94593aa84cdcb6474426fd9a6f
Chandan Janardan <chandanjanardan@gmail.com>
2023-05-21 19:37:01 +0530

    adding props

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Contactus } from './Contactus';
 import './App.css';
 
 function App() {
+  const contactdetial={
+    name:"Chandan",
+    phone:"11111111",
+    email:"email@gmail.com"
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Contactus name={contactdetial.name} phone={contactdetial.phone} email={contactdetial.email}/>
+    </>
   );
 }
 
---
+++ src/Contactus.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Contactus = (props) => {
+  return (
+    <div>
+        <h1>Name :{props.name}</h1>
+        <h1>Phone :{props.phone}</h1>
+        <h1>Email :{props.email}</h1>
+        </div>
+  )
+}
