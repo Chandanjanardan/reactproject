f904b059501991512f5cc5c70b9ad5744e4a298d
Chandan Janardan <chandanjanardan@gmail.com>
2023-06-04 19:45:22 +0530

    adding useContext

---
+++ reactrouter/src/context/ContextProvider.js
@@ -0,0 +1,16 @@
+import React, { Children } from 'react'
+import DataContext from './DataContext'
+
+
+
+// This will act as centalised component where we will keep everrything or every info related to webpage
+// A children is prop wich will become that component which is this component (context provider)wraps.
+function ContextProvider() {
+  return (
+    <DataContext.Provider>
+        {Children}
+    </DataContext.Provider>
+  )
+}
+
+export default ContextProvider
